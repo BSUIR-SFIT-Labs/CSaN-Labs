81ecb952d05b17aa7b55f529d9f463cccc988b42
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-02-16 19:32:29 +0300

    Implement output of all working groups and their resources

--- Lab_1/Lab_1/Lab_1.cpp
+++ Lab_1/Lab_1/Lab_1.cpp
@@ -1,12 +1,40 @@
 #pragma comment(lib, "Netapi32.lib")
+#pragma comment(lib, "mpr.lib")
 
+#include <Windows.h>
+#include <winnetwk.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
 #include <string>
-#include <Windows.h>
 
 using namespace std;
+
+void PrintAllActiveMacAddresses(void);
+BOOL WINAPI EnumerateFunc(LPNETRESOURCE lpnr);
+
+int main()
+{
+	// Output active MAC addresses.
+	cout << "Mac addresses:" << endl;
+	PrintAllActiveMacAddresses();
+	cout << "----------------------------------------" << endl;
+
+	// The output of all working groups on the network and their resources.
+	cout << "Working Groups:" << endl;
+
+	LPNETRESOURCE lpnr = NULL;
+
+	if (EnumerateFunc(lpnr) == FALSE)
+	{
+		cout << "Call to EnumerateFunc failed" << endl;
+	}
+	cout << "----------------------------------------" << endl << endl;
+
+	system("pause");
+	return 0;
+}
+
 
 bool ResetAdapter(int adapterNumber) 
 {
@@ -23,11 +51,11 @@ bool ResetAdapter(int adapterNumber)
 	return false;
 }
 
-bool GetAdapterMacAddress(int adapterNumber, string &macAddress) 
+BOOL GetAdapterMacAddress(int adapterNumber, string &macAddress) 
 {
 	if (ResetAdapter(adapterNumber))
 	{
-		// Prepare to get the adapter status block 
+		// Prepare to get the adapter status block .
 		NCB ncb;
 		memset(&ncb, 0, sizeof(ncb));
 		ncb.ncb_command = NCBASTAT;
@@ -54,16 +82,16 @@ bool GetAdapterMacAddress(int adapterNumber, string &macAddress)
 				int(adapterStatus.adapter_address[5]));
 			macAddress = bufferForMacAddress;
 
-			return true;
+			return TRUE;
 		}
 
-		return false;
+		return FALSE;
 	}
 
-	return false;
+	return FALSE;
 }
 
-LANA_ENUM GetAdapterList() 
+LANA_ENUM GetAdapterList(void) 
 {
 	LANA_ENUM adapterList;
 	NCB ncb;
@@ -78,11 +106,11 @@ LANA_ENUM GetAdapterList()
 	return adapterList;
 }
 
-void PrintAllMacAddresses()
+void PrintAllActiveMacAddresses(void)
 {
 	const LANA_ENUM adapterList = GetAdapterList();
 
-	// Get all of the local ethernet addresses
+	// Get all of the local ethernet addresses.
 	string macAddress;
 
 	for (int i = 0; i < adapterList.length; i++)
@@ -94,17 +122,171 @@ void PrintAllMacAddresses()
 		}
 		else
 		{
-			cerr << "Failed to get MAC address! Do you" << endl;
-			cerr << "have the NetBIOS protocol installed?" << endl;
+			cout << "Failed to get MAC address! Do you have the NetBIOS protocol installed?"
+				<< endl;
 			break;
 		}
 	}
 }
 
-int main()
+void DisplayStruct(int i, LPNETRESOURCE lpnrLocal)
 {
-	PrintAllMacAddresses();
+	cout << "NETRESOURCE[" << i << "] Scope: ";
+	switch (lpnrLocal->dwScope) {
+	case (RESOURCE_CONNECTED):
+		cout << "connected" << endl;
+		break;
+	case (RESOURCE_GLOBALNET):
+		cout << "all resources" << endl;
+		break;
+	case (RESOURCE_REMEMBERED):
+		cout << "remembered" << endl;
+		break;
+	default:
+		cout << "unknown scope " << lpnrLocal->dwScope << endl;
+		break;
+	}
 
-	system("pause");
-	return 0;
+	cout << "NETRESOURCE[" << i << "] Type: ";
+	switch (lpnrLocal->dwType) {
+	case (RESOURCETYPE_ANY):
+		cout << "any" << endl;
+		break;
+	case (RESOURCETYPE_DISK):
+		cout << "disk" << endl;
+		break;
+	case (RESOURCETYPE_PRINT):
+		cout << "print" << endl;
+		break;
+	default:
+		cout << "unknown type " << lpnrLocal->dwType << endl;
+		break;
+	}
+
+	cout << "NETRESOURCE[" << i << "] DisplayType: ";
+	switch (lpnrLocal->dwDisplayType) {
+	case (RESOURCEDISPLAYTYPE_GENERIC):
+		cout << "generic\n";
+		break;
+	case (RESOURCEDISPLAYTYPE_DOMAIN):
+		cout << "domain" << endl;
+		break;
+	case (RESOURCEDISPLAYTYPE_SERVER):
+		cout << "server" << endl;
+		break;
+	case (RESOURCEDISPLAYTYPE_SHARE):
+		cout << "share" << endl;
+		break;
+	case (RESOURCEDISPLAYTYPE_FILE):
+		cout << "file" << endl;
+		break;
+	case (RESOURCEDISPLAYTYPE_GROUP):
+		cout << "group" << endl;
+		break;
+	case (RESOURCEDISPLAYTYPE_NETWORK):
+		cout << "network" << endl;
+		break;
+	default:
+		cout << "unknown display type " << lpnrLocal->dwDisplayType << endl;
+		break;
+	}
+
+	cout << "NETRESOURCE[" << i << "] Usage: " << lpnrLocal->dwUsage << " = ";
+	if (lpnrLocal->dwUsage & RESOURCEUSAGE_CONNECTABLE)
+		cout << "connectable";
+	if (lpnrLocal->dwUsage & RESOURCEUSAGE_CONTAINER)
+		cout << "container";
+	cout << endl;
+
+	cout << "NETRESOURCE[" << i << "] Localname: " << lpnrLocal->lpLocalName << endl;
+	cout << "NETRESOURCE[" << i << "] Remotename: " << lpnrLocal->lpRemoteName << endl;
+	cout << "NETRESOURCE[" << i << "] Comment: " << lpnrLocal->lpComment << endl;
+	cout << "NETRESOURCE[" << i << "] Provider: " << lpnrLocal->lpProvider << endl;
+	cout << endl;
+}
+
+BOOL WINAPI EnumerateFunc(LPNETRESOURCE lpnr)
+{
+	DWORD dwResult, dwResultEnum;
+	HANDLE hEnum;
+	DWORD cbBuffer = 16384;
+	DWORD cEntries = -1;      
+	LPNETRESOURCE lpnrLocal;
+	DWORD i;
+   
+	// Call the WNetOpenEnum function to begin the enumeration.
+	dwResult = WNetOpenEnum(RESOURCE_GLOBALNET, // all network resources
+		RESOURCETYPE_ANY,                       // all resources
+		0,                                      // enumerate all resources
+		lpnr,                                   // NULL first time the function is called
+		&hEnum);                                // handle to the resource
+
+	if (dwResult != NO_ERROR) 
+	{
+		cout << "WnetOpenEnum failed with error " << dwResult << endl;
+		return FALSE;
+	}
+
+	// Call the GlobalAlloc function to allocate resources.
+	lpnrLocal = (LPNETRESOURCE)GlobalAlloc(GPTR, cbBuffer);
+	if (lpnrLocal == NULL) 
+	{
+		cout << "WnetOpenEnum failed with error " << dwResult << endl;
+		return FALSE;
+	}
+
+	do 
+	{
+		// Initialize the buffer.
+		ZeroMemory(lpnrLocal, cbBuffer);
+
+		// Call the WNetEnumResource function to continue
+		// the enumeration.
+		dwResultEnum = WNetEnumResource(hEnum,  // resource handle
+			&cEntries,                          // defined locally as -1
+			lpnrLocal,                          // LPNETRESOURCE
+			&cbBuffer);                         // buffer size
+
+		// If the call succeeds, loop through the structures.
+		if (dwResultEnum == NO_ERROR) 
+		{
+			for (i = 0; i < cEntries; i++) 
+			{
+
+				// Call an application-defined function to
+				// display the contents of the NETRESOURCE structures.
+				DisplayStruct(i, &lpnrLocal[i]);
+
+				// If the NETRESOURCE structure represents a container resource, 
+				// call the EnumerateFunc function recursively.
+				if (RESOURCEUSAGE_CONTAINER == (lpnrLocal[i].dwUsage
+					& RESOURCEUSAGE_CONTAINER))
+					if (!EnumerateFunc(&lpnrLocal[i]))
+						cout << "EnumerateFunc returned FALSE" << endl << endl;
+			}
+		}
+
+		// Process errors.
+		else if (dwResultEnum != ERROR_NO_MORE_ITEMS) 
+		{
+			cout << "WNetEnumResource failed with error " << dwResultEnum << endl;
+			break;
+		}
+	} while (dwResultEnum != ERROR_NO_MORE_ITEMS);
+
+	// Call the GlobalFree function to free the memory.
+	GlobalFree((HGLOBAL)lpnrLocal);
+
+	// Call WNetCloseEnum to end the enumeration.
+	dwResult = WNetCloseEnum(hEnum);
+
+	if (dwResult != NO_ERROR) {
+	
+		// Process errors.
+		cout << "WNetCloseEnum failed with error " << dwResult << endl;
+
+		return FALSE;
+	}
+
+	return TRUE;
 }
