e03ca134f48d2b01d4bedc2e397d8613bdceb1be
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-03-12 21:48:11 +0300

    Implement classes for working with a file sent over the network

---
+++ Lab_2/FileSenderLib/FileDetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileSenderLib
+{
+    [Serializable]
+    public class FileDetails
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public byte[] Content { get; set; }
+    }
+}
