c340b6f3e52528221c3051f08e26e8ea4afdc6e8
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-03-21 16:02:50 +0300

    Transfer the FileDetails.cs, TransmittedFile.cs files to the TFile folder

---
+++ Lab_2/FileSenderLib/TFile/FileDetails.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace FileSenderLib.TFile
+{
+    /// <summary>
+    /// Allows to work with file details.
+    /// </summary>
+    [Serializable]
+    public class FileDetails
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+
+        /// <summary>
+        /// Converts file details to an array of bytes.
+        /// </summary>
+        /// <param name="fileDetails">File details.</param>
+        /// <returns>Converted file details to byte array.</returns>
+        public static byte[] ConvertToByteArray(FileDetails fileDetails)
+        {
+            byte[] serializedFileDetails;
+
+            using (var memoryStream = new MemoryStream())
+            {
+                var xmlSerializer = new XmlSerializer(typeof(FileDetails));
+                xmlSerializer.Serialize(memoryStream, fileDetails);
+
+                memoryStream.Position = 0;
+                serializedFileDetails = new byte[memoryStream.Length];
+
+                const int memoryStreamOffset = 0;
+                memoryStream.Read(serializedFileDetails, memoryStreamOffset,
+                    serializedFileDetails.Length);
+            }
+
+            return serializedFileDetails;
+        }
+
+        /// <summary>
+        /// Converts a byte array containing file details to file details.
+        /// </summary>
+        /// <param name="byteArrayFileDetails">File details represented as an array of bytes.</param>
+        /// <returns>File details</returns>
+        public static FileDetails ConvertToFileDetails(byte[] byteArrayFileDetails)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                const int memoryStreamOffset = 0;
+
+                memoryStream.Write(byteArrayFileDetails, memoryStreamOffset,
+                    byteArrayFileDetails.Length);
+                memoryStream.Position = 0;
+
+                var xmlSerializer = new XmlSerializer(typeof(FileDetails));
+                return (FileDetails)xmlSerializer.Deserialize(memoryStream);
+            }
+        }
+    }
+}
---
+++ Lab_2/FileSenderLib/TFile/TransmittedFile.cs
@@ -0,0 +1,98 @@
+﻿using System.IO;
+
+namespace FileSenderLib.TFile
+{
+    /// <summary>
+    /// Allows to work with a file sent over the network.
+    /// </summary>
+    public class TransmittedFile
+    {
+        private readonly FileDetails _fileDetails;
+        private readonly string _pathToFile;
+
+        /// <summary>
+        /// Initializes a file details information field.
+        /// </summary>
+        private TransmittedFile()
+        {
+            _fileDetails = new FileDetails();
+        }
+
+        /// <summary>
+        /// Sets the path to the file sent over the network and file details.
+        /// </summary>
+        /// <param name="pathToFile">The path to the file to be sent over the network.</param>
+        public TransmittedFile(string pathToFile) : this()
+        {
+            _pathToFile = pathToFile;
+
+
+            var fileInfo = new FileInfo(pathToFile);
+            _fileDetails.Name = Path.GetFileNameWithoutExtension(fileInfo.Name);
+            _fileDetails.Extension = Path.GetExtension(fileInfo.Name);
+        }
+
+        /// <summary>
+        /// Sets file details.
+        /// </summary>
+        /// <param name="byteArrayFileDetails">File details represented as an array of bytes.</param>
+        public TransmittedFile(byte[] byteArrayFileDetails) : this()
+        {
+            _fileDetails = FileDetails.ConvertToFileDetails(byteArrayFileDetails);
+        }
+
+        /// <summary>
+        /// Returns file details as an array of bytes.
+        /// </summary>
+        /// <returns>file details represented as an array of bytes.</returns>
+        public byte[] GetByteArrayFileDetails()
+        {
+            return FileDetails.ConvertToByteArray(_fileDetails);
+        }
+
+        /// <summary>
+        /// Returns the length of the file content.
+        /// </summary>
+        /// <returns>The length of the file content.</returns>
+        public long GetFileContentLength()
+        {
+            var fileInfo = new FileInfo(_pathToFile);
+            return fileInfo.Length;
+        }
+
+        /// <summary>
+        /// Reads a file bytes into the buffer.
+        /// </summary>
+        /// <param name="buffer">Buffer for reading a file.</param>
+        /// <param name="bufferOffset">The offset from the beginning of the buffer.</param>
+        /// <param name="count">The number of bytes to read.</param>
+        /// <param name="fileOffset">Offset from the beginning of the file.</param>
+        public void ReadBytes(byte[] buffer, int bufferOffset, int count, long fileOffset)
+        {
+            using (var fileStream = new FileStream(_pathToFile,
+                FileMode.Open, FileAccess.Read))
+            {
+                fileStream.Seek(fileOffset, SeekOrigin.Begin);
+
+                fileStream.Read(buffer, bufferOffset, count);
+            }
+        }
+
+        /// <summary>
+        /// Writes bytes from the buffer to the file.
+        /// </summary>
+        /// <param name="pathToFolder">The path to the folder with the file.</param>
+        /// <param name="content">An array of bytes to write to the file.</param>
+        /// <param name="bufferOffset">The offset from the beginning of the buffer.</param>
+        /// <param name="count">The number of bytes to write.</param>
+        public void WriteBytes(string pathToFolder, byte[] content, int bufferOffset, int count)
+        {
+            using (var fileStream = new FileStream(pathToFolder + _fileDetails.Name
+                + _fileDetails.Extension, FileMode.Append,
+                FileAccess.Write, FileShare.Write))
+            {
+                fileStream.Write(content, bufferOffset, count);
+            }
+        }
+    }
+}
