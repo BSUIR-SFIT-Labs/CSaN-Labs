66bb3ed23af437e3d041e20195a2ce624e132fe3
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-03-21 16:07:49 +0300

    Redo the program for sending files over the network

--- Lab_2/Sender/Sender.cs
+++ Lab_2/Sender/Sender.cs
@@ -5,18 +5,43 @@ namespace Sender
 {
     class Sender
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Enter path to file: ");
-            string pathToFile = Console.ReadLine();
+            Console.Write("Enter the IP address: ");
+            string ipAddress = Console.ReadLine();
 
-            var tcpSender = new TcpSender();
-            tcpSender.Start("127.0.0.1", 13);
-            tcpSender.SendFile(pathToFile);
-            tcpSender.Stop();
+            Console.Write("Enter port: ");
+            int port;
+            try
+            {
+                port = Convert.ToInt32(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Error: need to enter a number!");
+                return 1;
+            }
 
-            Console.WriteLine("END!");
+            try
+            {
+                var tcpSender = new TcpSender();
+                tcpSender.Start(ipAddress, port);
+
+                Console.Write("Enter path to file: ");
+                string pathToFile = Console.ReadLine();
+
+                tcpSender.SendFile(pathToFile);
+
+                tcpSender.Stop();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Stop!");
             Console.ReadLine();
+            return 0;
         }
     }
 }
