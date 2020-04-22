b05c019f329203ffce5a51f033d32f41b2f8462c
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-04-22 22:18:56 +0300

    Implement the ability to send a query string to the server

---
+++ Lab_4/HttpLib/ExceptionMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace HttpLib
+{
+    class ExceptionMessages
+    {
+        public const string IOExceptionMessage = "Unable to read data from the transport connection: An established connection was aborted by the software in your host machine.";
+    }
+}
---
+++ Lab_4/HttpLib/HttpClient.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace HttpLib
+{
+    public class HttpClient : IDisposable
+    {
+        public const int HttpPort = 80;
+
+        private readonly TcpClient _tcpClient;
+        private NetworkStream _networkStream;
+
+        public HttpClient(string hostname)
+        {
+            _tcpClient = new TcpClient(hostname, HttpPort);
+        }
+
+        public string SendRequest(string query)
+        {
+            _networkStream = _tcpClient.GetStream();
+
+            _networkStream.Write(Encoding.ASCII.GetBytes(query));
+
+            byte[] buffer = new byte[_tcpClient.ReceiveBufferSize];
+
+            int currentNumOfBytesRead;
+            var stringBuilder = new StringBuilder();
+            do
+            {
+                try
+                {
+                    currentNumOfBytesRead = _networkStream.Read(buffer);
+                }
+                catch (IOException)
+                {
+                    Dispose();
+                    throw new IOException(ExceptionMessages.IOExceptionMessage);
+                }
+
+                if (currentNumOfBytesRead == 0)
+                {
+                    break;
+                }
+
+                stringBuilder.AppendLine(Encoding.ASCII.GetString(buffer, 0,
+                    currentNumOfBytesRead));
+                Array.Clear(buffer, 0, buffer.Length);
+            }
+            while (currentNumOfBytesRead == buffer.Length);
+
+            return stringBuilder.ToString();
+        }
+
+        public void Dispose()
+        {
+            _networkStream?.Close();
+            _tcpClient?.Close();
+        }
+    }
+}
