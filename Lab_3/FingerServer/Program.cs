87a242a48b30d14acafa57beb58d380732151902
Shulga Konstantin <mrkonstantinsh@gmail.com>
2020-04-25 22:16:46 +0300

    Implement the server side of the Finger protocol

--- Lab_3/FingerServer/Program.cs
+++ Lab_3/FingerServer/Program.cs
@@ -6,7 +6,11 @@ namespace FingerServer
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Server is running!");
+            Console.WriteLine("Waiting for user connection...");
+
+            var server = new FingerLib.Server.FingerServer();
+            server.Start("127.0.0.1");
         }
     }
 }
